17b76bf40d02f3d69fab39bd45101e9268b19b78
xueyongfu.xyf <xueyongfu.xyf@alibaba-inc.com>
2015-06-03 15:14:06 +0800

    test

--- src/cn/safei/utils/activity/MainActivity.java
+++ src/cn/safei/utils/activity/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        T.show(this, "heeo", 0);
+        T.show(this, "master", 0);
     }
 
     @Override
