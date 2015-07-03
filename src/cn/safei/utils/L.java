5f883cd8d764bb36882456e1da443a9423096fda
xueyongfu.xyf <xueyongfu.xyf@alibaba-inc.com>
2015-07-03 10:53:52 +0800

    增加格式化的打印接口
    用法：L.d("%s %03d", "hello world!", 1);

--- src/cn/safei/utils/L.java
+++ src/cn/safei/utils/L.java
@@ -2,6 +2,8 @@ package cn.safei.utils;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Formatter;
+import java.util.Locale;
 
 import android.util.Log;
 
@@ -26,7 +28,313 @@ public class L {
     public static boolean isDebug = true;// 是否需要打印bug，可以在application的onCreate函数里面初始化
     private static String TAG = "sanbo";
 
-    // 下面四个是默认tag的函数
+    /**********************************************************************/
+    /**
+     *
+     * @param l
+     *            The {@linkplain java.util.Locale locale} to apply during
+     *            formatting. If {@code l} is {@code null} then no localization
+     *            is applied.
+     * @param format
+     *            A <a href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >format string</a>
+     * @param args
+     *            Arguments referenced by the format specifiers in the format
+     *            string. If there are more arguments than format specifiers,
+     *            the extra arguments are ignored. The number of arguments is
+     *            variable and may be zero. The maximum number of arguments is
+     *            limited by the maximum dimension of a Java array as defined by
+     *            <cite>The Java&trade; Virtual Machine Specification</cite>.
+     *            The behaviour on a {@code null} argument depends on the <a
+     *            href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >conversion</a>.
+     * @throws java.util.IllegalFormatException
+     *             If a format string contains an illegal syntax, a format
+     *             specifier that is incompatible with the given arguments,
+     *             insufficient arguments given the format string, or other
+     *             illegal conditions. For specification of all possible
+     *             formatting errors, see the <a href=
+     *             "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#detail"
+     *             >Details</a> section of the formatter class specification
+     *
+     */
+    public static void i(Locale l, String format, Object... args) {
+        if (isDebug) {
+            try {
+                String msg = new Formatter(l).format(format, args).toString();
+                i(TAG, msg);
+            } catch (Throwable e) {
+                e(e);
+            }
+        }
+    }
+
+    /**
+     *
+     * @param l
+     *            The {@linkplain java.util.Locale locale} to apply during
+     *            formatting. If {@code l} is {@code null} then no localization
+     *            is applied.
+     * @param format
+     *            A <a href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >format string</a>
+     * @param args
+     *            Arguments referenced by the format specifiers in the format
+     *            string. If there are more arguments than format specifiers,
+     *            the extra arguments are ignored. The number of arguments is
+     *            variable and may be zero. The maximum number of arguments is
+     *            limited by the maximum dimension of a Java array as defined by
+     *            <cite>The Java&trade; Virtual Machine Specification</cite>.
+     *            The behaviour on a {@code null} argument depends on the <a
+     *            href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >conversion</a>.
+     * @throws java.util.IllegalFormatException
+     *             If a format string contains an illegal syntax, a format
+     *             specifier that is incompatible with the given arguments,
+     *             insufficient arguments given the format string, or other
+     *             illegal conditions. For specification of all possible
+     *             formatting errors, see the <a href=
+     *             "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#detail"
+     *             >Details</a> section of the formatter class specification
+     *
+     */
+    public static void d(Locale l, String format, Object... args) {
+        if (isDebug) {
+            try {
+                String msg = new Formatter(l).format(format, args).toString();
+                d(TAG, msg);
+            } catch (Throwable e) {
+                e(e);
+            }
+        }
+    }
+
+    /**
+     *
+     * @param l
+     *            The {@linkplain java.util.Locale locale} to apply during
+     *            formatting. If {@code l} is {@code null} then no localization
+     *            is applied.
+     * @param format
+     *            A <a href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >format string</a>
+     * @param args
+     *            Arguments referenced by the format specifiers in the format
+     *            string. If there are more arguments than format specifiers,
+     *            the extra arguments are ignored. The number of arguments is
+     *            variable and may be zero. The maximum number of arguments is
+     *            limited by the maximum dimension of a Java array as defined by
+     *            <cite>The Java&trade; Virtual Machine Specification</cite>.
+     *            The behaviour on a {@code null} argument depends on the <a
+     *            href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >conversion</a>.
+     * @throws java.util.IllegalFormatException
+     *             If a format string contains an illegal syntax, a format
+     *             specifier that is incompatible with the given arguments,
+     *             insufficient arguments given the format string, or other
+     *             illegal conditions. For specification of all possible
+     *             formatting errors, see the <a href=
+     *             "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#detail"
+     *             >Details</a> section of the formatter class specification
+     *
+     */
+    public static void e(Locale l, String format, Object... args) {
+        if (isDebug) {
+            try {
+                String msg = new Formatter(l).format(format, args).toString();
+                e(TAG, msg);
+            } catch (Throwable e) {
+                e(e);
+            }
+        }
+    }
+
+    /**
+     *
+     * @param l
+     *            The {@linkplain java.util.Locale locale} to apply during
+     *            formatting. If {@code l} is {@code null} then no localization
+     *            is applied.
+     * @param format
+     *            A <a href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >format string</a>
+     * @param args
+     *            Arguments referenced by the format specifiers in the format
+     *            string. If there are more arguments than format specifiers,
+     *            the extra arguments are ignored. The number of arguments is
+     *            variable and may be zero. The maximum number of arguments is
+     *            limited by the maximum dimension of a Java array as defined by
+     *            <cite>The Java&trade; Virtual Machine Specification</cite>.
+     *            The behaviour on a {@code null} argument depends on the <a
+     *            href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >conversion</a>.
+     * @throws java.util.IllegalFormatException
+     *             If a format string contains an illegal syntax, a format
+     *             specifier that is incompatible with the given arguments,
+     *             insufficient arguments given the format string, or other
+     *             illegal conditions. For specification of all possible
+     *             formatting errors, see the <a href=
+     *             "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#detail"
+     *             >Details</a> section of the formatter class specification
+     *
+     */
+    public static void v(Locale l, String format, Object... args) {
+        if (isDebug) {
+            try {
+                String msg = new Formatter(l).format(format, args).toString();
+                v(TAG, msg);
+            } catch (Throwable e) {
+                e(e);
+            }
+        }
+    }
+
+    /**********************************************************************/
+    /**
+     *
+     * @param format
+     *            A <a href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >format string</a>
+     *
+     * @param args
+     *            * Arguments referenced by the format specifiers in the format
+     *            string. If there are more arguments than format specifiers,
+     *            the extra arguments are ignored. The number of arguments is
+     *            variable and may be zero. The maximum number of arguments is
+     *            limited by the maximum dimension of a Java array as defined by
+     *            <cite>The Java&trade; Virtual Machine Specification</cite>.
+     *            The behaviour on a {@code null} argument depends on the <a
+     *            href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >conversion</a>.
+     * @throws java.util.IllegalFormatException
+     *             If a format string contains an illegal syntax, a format
+     *             specifier that is incompatible with the given arguments,
+     *             insufficient arguments given the format string, or other
+     *             illegal conditions. For specification of all possible
+     *             formatting errors, see the <a href=
+     *             "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#detail"
+     *             >Details</a> section of the formatter class specification.
+     */
+    public static void i(String format, Object... args) {
+        if (isDebug) {
+            String msg = new Formatter().format(format, args).toString();
+            i(TAG, msg);
+        }
+    }
+
+    /**
+     *
+     * @param format
+     *            A <a href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >format string</a>
+     *
+     * @param args
+     *            * Arguments referenced by the format specifiers in the format
+     *            string. If there are more arguments than format specifiers,
+     *            the extra arguments are ignored. The number of arguments is
+     *            variable and may be zero. The maximum number of arguments is
+     *            limited by the maximum dimension of a Java array as defined by
+     *            <cite>The Java&trade; Virtual Machine Specification</cite>.
+     *            The behaviour on a {@code null} argument depends on the <a
+     *            href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >conversion</a>.
+     * @throws java.util.IllegalFormatException
+     *             If a format string contains an illegal syntax, a format
+     *             specifier that is incompatible with the given arguments,
+     *             insufficient arguments given the format string, or other
+     *             illegal conditions. For specification of all possible
+     *             formatting errors, see the <a href=
+     *             "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#detail"
+     *             >Details</a> section of the formatter class specification.
+     */
+    public static void d(String format, Object... args) {
+        if (isDebug) {
+            String msg = new Formatter().format(format, args).toString();
+            d(TAG, msg);
+        }
+    }
+
+    /**
+     *
+     * @param format
+     *            A <a href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >format string</a>
+     *
+     * @param args
+     *            * Arguments referenced by the format specifiers in the format
+     *            string. If there are more arguments than format specifiers,
+     *            the extra arguments are ignored. The number of arguments is
+     *            variable and may be zero. The maximum number of arguments is
+     *            limited by the maximum dimension of a Java array as defined by
+     *            <cite>The Java&trade; Virtual Machine Specification</cite>.
+     *            The behaviour on a {@code null} argument depends on the <a
+     *            href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >conversion</a>.
+     * @throws java.util.IllegalFormatException
+     *             If a format string contains an illegal syntax, a format
+     *             specifier that is incompatible with the given arguments,
+     *             insufficient arguments given the format string, or other
+     *             illegal conditions. For specification of all possible
+     *             formatting errors, see the <a href=
+     *             "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#detail"
+     *             >Details</a> section of the formatter class specification.
+     */
+    public static void e(String format, Object... args) {
+        if (isDebug) {
+            String msg = new Formatter().format(format, args).toString();
+            e(TAG, msg);
+        }
+    }
+
+    /**
+     *
+     * @param format
+     *            A <a href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >format string</a>
+     *
+     * @param args
+     *            * Arguments referenced by the format specifiers in the format
+     *            string. If there are more arguments than format specifiers,
+     *            the extra arguments are ignored. The number of arguments is
+     *            variable and may be zero. The maximum number of arguments is
+     *            limited by the maximum dimension of a Java array as defined by
+     *            <cite>The Java&trade; Virtual Machine Specification</cite>.
+     *            The behaviour on a {@code null} argument depends on the <a
+     *            href=
+     *            "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#syntax"
+     *            >conversion</a>.
+     * @throws java.util.IllegalFormatException
+     *             If a format string contains an illegal syntax, a format
+     *             specifier that is incompatible with the given arguments,
+     *             insufficient arguments given the format string, or other
+     *             illegal conditions. For specification of all possible
+     *             formatting errors, see the <a href=
+     *             "http://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html#detail"
+     *             >Details</a> section of the formatter class specification.
+     */
+    public static void v(String format, Object... args) {
+        if (isDebug) {
+            String msg = new Formatter().format(format, args).toString();
+            v(TAG, msg);
+        }
+    }
+
+    /**********************************************************************/
     public static void i(String msg) {
         if (isDebug)
             i(TAG, msg);
@@ -57,6 +365,7 @@ public class L {
             e(TAG, null, e);
     }
 
+    /**********************************************************************/
     // 下面是传入自定义tag的函数
     public static void i(String tag, String msg) {
         if (isDebug)
@@ -89,6 +398,7 @@ public class L {
         TAG = tag;
     }
 
+    /**********************************************************************/
     /**
      * 将error转换成字符串
      */
